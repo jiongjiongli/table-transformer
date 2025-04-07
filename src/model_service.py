9c334bf5bc424af0d355127cc9d36b2eed44b0f2
jiongjiongli <33146359+jiongjiongli@users.noreply.github.com>
2025-04-06 21:45:15 -0500

    Update model_service.py

--- src/model_service.py
+++ src/model_service.py
@@ -3,8 +3,11 @@ import argparse
 import os
 from pathlib import Path
 import sys
-sys.path.append("../scripts")
 from PIL import Image
+
+import_scripts_path = Path(__file__).resolve().parent / "../scripts"
+print(f"Importing {import_scripts_path}...")
+sys.path.append(str(import_scripts_path))
 
 from generate_ocr_words import tesseract_ocr
 from inference import TableExtractionPipeline, output_result
